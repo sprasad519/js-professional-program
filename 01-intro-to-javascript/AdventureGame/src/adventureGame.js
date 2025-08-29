569b0dddad2b3375779e65dce05ee32675bdaf93
SIVAPRASAD G <sprasadg519@outlook.com>
2025-08-29 09:56:26 +0530

    added AdventureGame pro

---
+++ 01-intro-to-javascript/AdventureGame/src/adventureGame.js
@@ -0,0 +1,11 @@
+// Adventure Game
+// This game will be a text-based adventure game where the player will be able
+// to make choices that affect the outcome of the game.
+// The player will be able to choose their own path and the story will change
+// based on their decisions.
+
+//Display the game title
+console.log("Welcome to the Adventure Game!");
+
+// Add a welcome message
+console.log("Prepare yourself for an epic journey!");
